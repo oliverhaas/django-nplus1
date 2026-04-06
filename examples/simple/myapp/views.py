1f65f6b4b5b80c11a6a13221fcbe58c23ef0b40f
Oliver Haas <ohaas@e1plus.de>
2026-04-06 11:37:12 +0200

    docs: restructure example with service layer and nplus1_allow in tests
    - BookService with single-instance and batch methods
    - nplus1_allow used in TESTS only, not in production code
    - Batch method uses prefetch_related_objects (always N+1-safe)
    - Single-instance method delegates prefetching to caller
    - Three view patterns: bad (N+1), good (select_related), good batch

--- examples/simple/myapp/views.py
+++ examples/simple/myapp/views.py
@@ -1,18 +1,24 @@
 from django.http import JsonResponse
 
 from myapp.models import Book
-from myapp.services import get_author_names
+from myapp.services import BookService
 
 
 def book_list_bad(request):
     """N+1: fetches all books then accesses author on each without prefetching."""
     books = list(Book.objects.all())
-    # Accessing author directly here triggers N+1
-    authors = [book.author.name for book in books]
+    authors = [BookService.book_get_author_name(book=book) for book in books]
     return JsonResponse({"authors": authors})
 
 
 def book_list_good(request):
-    """Fixed: prefetches authors at the view layer, then delegates to service."""
+    """Fixed: prefetches authors at the view layer."""
     books = list(Book.objects.select_related("author"))
-    return JsonResponse({"authors": get_author_names(books)})
+    authors = [BookService.book_get_author_name(book=book) for book in books]
+    return JsonResponse({"authors": authors})
+
+
+def book_list_good_batch(request):
+    """Fixed: uses batch service method which prefetches internally."""
+    books = list(Book.objects.all())
+    return JsonResponse({"authors": BookService.book_get_author_names(books=books)})
